145028e9450b23b2f32354450bba058294d41277
Ali Jetham <alijetham24@gmail.com>
2025-05-29 21:43:00 +0530

    chore(web): setup project structure and dedendencies for apps/web

---
+++ apps/web/src/app/App.tsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+    <div className="min-h-screen min-w-screen bg-purple-500">
+      <h1 className="text-2xl md:text-3xl lg:text-4xl">test</h1>
+    </div>
+
+  )
+}
+
+export default App
